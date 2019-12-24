ad0669612daff40ea106437d6ecaedba39d352b2
pjx1211 <497421855@qq.com>
2019-12-24 20:43:45 +0800

    开发

--- admin/src/views/articleList.vue
+++ admin/src/views/articleList.vue
@@ -1,19 +1,37 @@
 //显示所有文章列表组件
 <template>
 	<div class="article-list">
-		<p>文章列表（共12篇文章）</p>
+		<div class="btn-items flex">
+			<div class="search">
+				<label for="userName">用户名称：</label>
+				<el-input size="mini" type="text" id="userName" name="userName" placeholder="请输入用户名"></el-input>
+			</div>
+			<el-button type="primary" size="mini" icon="el-icon-search">搜索</el-button>
+			<el-button size="mini">重置</el-button>
+			<el-button size="mini" icon="el-icon-arrow-down">展开</el-button>
+		</div>
+		<div class="btn-items">
+			<el-button type="primary" size="mini" icon="el-icon-plus">添加用户</el-button>
+			<el-button size="mini" icon="el-icon-delete">批量删除</el-button>
+		</div>
 		<div class="article-table-wrap">
 			<div class="table">
 				<el-table :data="tableData" show-summary border stripe style="width: 100%" size="mini">
-					<el-table-column v-for="item in tableHeader" :label="item.label" :prop="item.prop" show-overflow-tooltip width="auto">
-						
-					</el-table-column>
+					<el-table-column type="selection" width="55"></el-table-column>
+					<el-table-column
+						v-for="item in tableHeader"
+						:label="item.label"
+						:prop="item.prop"
+						align="center"
+						show-overflow-tooltip
+						width="auto"
+					></el-table-column>
 					<!-- <el-table-column label="状态">
 						<template slot-scope="scope">
 							<el-tag :type="true ? 'primary' : 'success'" size="mini" disable-transitions>已使用</el-tag>
 						</template>
-					</el-table-column> -->
-					<el-table-column label="操作" width="200">
+					</el-table-column>-->
+					<el-table-column label="操作" width="200" align="center">
 						<template slot-scope="scope">
 							<el-button size="mini" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
 							<el-button
@@ -27,7 +45,7 @@
 				</el-table>
 			</div>
 			<!-- 分页 -->
-			<div class="pagination">
+			<div class="pagination" v-show='tableData.length !==0'>
 				<el-pagination
 					:page-sizes="[100, 200, 300, 400]"
 					:page-size="100"
@@ -45,67 +63,25 @@ export default {
 	data() {
 		return {
 			pageSize: 10, //每页显示9条数据
-			tableHeader:[
+			tableHeader: [
 				{
-					label:'1',
-					prop:'day'
+					label: '姓名',
+					prop: 'eName'
 				},
 				{
-					label:'2',
-					prop:'daySalary'
+					label: '天数',
+					prop: 'eDay'
 				},
 				{
-					label:'3',
-					prop:'pname'
+					label: '说明',
+					prop: 'mark'
 				},
 				{
-					label:'4',
-					prop:'ename'
+					label: '逻辑删除',
+					prop: 'logicDel'
 				}
 			],
 			tableData: [
-                {
-                    day:1,
-                    daySalary:23,
-                    pname:"222",
-                    ename:'sss'
-                },
-                {
-                    day:1,
-                    daySalary:23,
-                    pname:"222",
-                    ename:'sss'
-                },
-                {
-                    day:1,
-                    daySalary:23,
-                    pname:"222",
-                    ename:'sss'
-                },
-                {
-                    day:1,
-                    daySalary:23,
-                    pname:"222",
-                    ename:'sss'
-                },
-                {
-                    day:1,
-                    daySalary:23,
-                    pname:"222",
-                    ename:'sss'
-                },
-                {
-                    day:1,
-                    daySalary:23,
-                    pname:"222",
-                    ename:'sss'
-                },
-                {
-                    day:1,
-                    daySalary:23,
-                    pname:"222",
-                    ename:'sss'
-                }
 			]
 		}
 	},
@@ -131,10 +107,25 @@ export default {
 </script>
 
 <style lang='less' scoped>
+label {
+	white-space: nowrap;
+}
+.btn-items {
+	margin: 10px !important;
+}
+.search {
+	width: 20%;
+	display: flex;
+	align-items: center;
+	margin-right: 10px;
+}
 .article-list {
 	background-color: #fff;
 	width: 100%;
+	overflow: hidden;
 	height: 100%;
+	display: flex;
+	flex-direction: column;
 
 	& > p {
 		padding: 10px;
@@ -146,7 +137,6 @@ export default {
 	}
 }
 .article-table-wrap {
-	padding: 50px 0 40px 0;
 	width: 100%;
 	box-sizing: border-box;
 	animation: show 0.8s;
@@ -154,10 +144,12 @@ export default {
 	height: 100%;
 
 	.table {
-        width: 100%;
-        height: 100%;
-        position: relative;
-        overflow: auto;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		overflow: auto;
+		box-sizing: border-box;
+		padding-bottom: 40px;
 	}
 }
 
@@ -169,5 +161,9 @@ export default {
 			color: #29b6f6;
 		}
 	}
+}
+.pagination {
+	position: absolute;
+	bottom: 0;
 }
 </style>
